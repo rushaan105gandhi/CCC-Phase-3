166306da6bc06dac28163405ac315c701870f3a7
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-30 13:24:19 +0530

    ted mobsy and candles

---
+++ ccc-phase-3/ted_mobsy_and_candles.py
@@ -0,0 +1,22 @@
+def binary_search(arr, x):
+    l, r = 0, len(arr)-1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == x:
+            return True
+        elif arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return False
+
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+C = list(map(int, input().split()))
+
+for i in range(M):
+    if binary_search(A, C[i]):
+        print("Happy Halloween!")
+    else:
+        print("Tricky!")
